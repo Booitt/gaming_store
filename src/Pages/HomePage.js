03d74110cc1abee34122968d1d12aacc84ec7a05
Booit Desktop <allisson-xd@hotmail.com>
2021-07-14 13:20:41 -0300

    some changes

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,16 @@
 import Section from "../components/Section";
+import backgroundImage from "../assets/images/bg1.jpg"
 
 const HomePage = () => {
 	return (
 		<main>
-			<Section>
-				<h1>Hello World</h1>
-			</Section>
+			<header></header>
+			<main>
+				<Section backgroundImage={backgroundImage}>
+					<h1>Hello World</h1>
+				</Section>
+			</main>
+			<footer></footer>
 		</main>
 	);
 };
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,13 +1,19 @@
 import styled from "styled-components";
 
 const StyledSection = styled.section`
-    height: 100vh;
-    width: auto;
-    padding: .5rem 1rem;
+	background: url(${(props) => props.backgroundImage}) no-repeat;
+    background-size: cover;
+	height: 100vh;
+	width: auto;
+	padding: 0.5rem 1rem;
 `;
 
 const Section = (props) => {
-    return <StyledSection>{props.children}</StyledSection>
-}
+	return (
+		<StyledSection {...props}>
+			{props.children}
+		</StyledSection>
+	);
+};
 
 export default Section;
